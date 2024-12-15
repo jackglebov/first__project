3220892d67219ac21e470a358bd6cc3441b1adbf
jackglebov <7295044@gmail.com>
2024-12-15 14:03:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+import tkinter as tk
+
+win = tk.Tk()
+canvas = tk.Canvas(win,bg ="white", height=400, width=400)
+for i in range(50,400,50):
+    canvas.create_line((0,i), (400,i))
+    canvas.create_line((i, 0), (i, 400))
+    canvas.pack()
+canvas.mainloop()
