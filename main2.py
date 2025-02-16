76fd7fbaeeba86dd4899abcd1008889ade5e4cce
jackglebov <7295044@gmail.com>
2025-02-16 14:09:44 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,14 +1,27 @@
-def klava(e):
+import pygame as py
+from random import *
+py.init()
+speed = 0.1
+x=0
+FPS=30
+win = py.display.set_mode((500, 600))
 
-    if e.keysym == "Up":
-        canvas.move(oval,0,-50)
-    if e.keysym == "Down":
-        canvas.move(oval, 0, 50)
-import tkinter as tk
-win = tk.Tk()
-canvas = tk.Canvas(win,bg ="white", height=400, width=400)
-pula=canvas.create_oval((500,500),(500,500),fill="yellow")
-oval=    canvas.create_oval((0,0),(50,50),fill="red")
-canvas.pack()
-win.bind("<KeyPress>",klava)
-canvas.mainloop()
+'''
+for i in range(1,100,1):
+    x = randint(1, 255)
+    z = randint(1, 255)
+    c= randint(1, 255)
+'''
+clock = py.time.Clock()
+while True:
+    win.fill((255, 255, 255))
+    py.draw.circle(win ,(255,0,0),(x,250),24)
+    x=x+speed
+    py.display.update()
+    if x  >500:
+        speed=-0.1
+    if x < 0:
+        speed = 0.1
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            exit()
